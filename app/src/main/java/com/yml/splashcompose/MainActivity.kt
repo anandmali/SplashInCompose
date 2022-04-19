590ce0d3a9b840ce310e8280752fc0f48c7a394e
anandmali <anandm475@gmail.com>
2022-04-19 14:53:13 +0530

    Initial splash API setup

---
+++ app/src/main/java/com/yml/splashcompose/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.yml.splashcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.yml.splashcompose.ui.theme.SplashComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        // Handle the splash screen transition.
+        val splashScreen = installSplashScreen()
+
+        splashScreen.apply {
+            setKeepOnScreenCondition() {
+                true
+            }
+        }
+
+        setContent {
+            SplashComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SplashComposeTheme {
+        Greeting("Android")
+    }
+}
